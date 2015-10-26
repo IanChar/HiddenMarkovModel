fded702a27be300176d71423bfd278fb91969ad9
Ian Char <ianchar4@gmail.com>
2015-10-25 20:34:46 -0600

    Fixed a bug

--- HiddenMarkovModel.py
+++ HiddenMarkovModel.py
@@ -9,7 +9,15 @@ class HiddenMarkovModel(object):
         self.p = p
         self.q = q
 
-    def calculateYPermutation(self, yPerm):
+    def monteCarloYPathProbability(self, yPerm, trials):
+        counter = 0
+        for _ in range(trials):
+            randPerm = self.yStatesUpTo(len(yPerm) - 1)
+            if yPerm == randPerm:
+                counter += 1
+        return counter
+
+    def calculateYPathProbability(self, yPerm):
         n = len(yPerm)
         # Generate all x permutations
         def generatePermutation(n, numStates, currPerm = [], acc = []):
@@ -19,13 +27,14 @@ class HiddenMarkovModel(object):
                 acc += generatePermutation(n, numStates, currPerm + [i], [])
             return acc
         xPerms = generatePermutation(len(yPerm), int(p.shape[0]))
+        print xPerms
         result = 0
         for perm in xPerms:
             component = mu.item(perm[0] - 1)
             for i in range(len(perm) - 1):
                 component *= p.item(perm[i] - 1, perm[i+1] - 1)
             for i in range(len(perm)):
-                component *= p.item(perm[i] - 1, yPerm[i] - 1)
+                component *= q.item(perm[i] - 1, yPerm[i] - 1)
             result += component
         return float(result)
 
@@ -33,6 +42,7 @@ class HiddenMarkovModel(object):
         xStates = self.xStatesUpTo(n)
         yStates = []
         for x in xStates:
+            print matrix([[q.item(x - 1, 0), q.item(x-1, 1)]])
             yVec = matrix([[q.item(x - 1, 0), q.item(x-1, 1)]])
             yStates.append(self.discreteInverse(yVec))
         return yStates
@@ -45,7 +55,7 @@ class HiddenMarkovModel(object):
             if currPtm is None:
                 currPtm = p
             else:
-                currPtm *= p
+                currPtm = currPtm * p
             v = mu * currPtm
             states.append(self.discreteInverse(v))
         return states
@@ -62,17 +72,20 @@ class HiddenMarkovModel(object):
                 return state
 
 if __name__ == '__main__':
-    mu = matrix([[Fraction(1, 6), Fraction(2,6), Fraction(3, 6)]])
-    p = matrix([[Fraction(0,1), Fraction(2, 3), Fraction(1, 3)],
-                [Fraction(1,3), Fraction(1,3), Fraction(1,3)],
-                [Fraction(1,3), Fraction(2,3), Fraction(0,3)]])
-    q = matrix([[Fraction(1,1), Fraction(0,1)],
-                [Fraction(1,2), Fraction(1,2)],
-                [Fraction(0,1), Fraction(1,1)]])
+    mu = matrix([[float(1)/6, float(1)/3, float(1)/2]])
+    p = matrix([[0, float(2)/ 3, float(1)/ 3],
+                [float(1)/3, float(1)/3, float(1)/3],
+                [float(1)/3, float(2)/3, 0]])
+    q = matrix([[1, 0],
+                [float(1)/2, float(1)/2],
+                [0, 1]])
     hmm = HiddenMarkovModel(mu, p, q)
     # 3.1
     print hmm.xStatesUpTo(3)
     # 3.2
     print hmm.yStatesUpTo(3)
     # 3.3
-    print hmm.calculateYPermutation([2,1,2])
+    # print hmm.calculateYPathProbability([2,1,2])
+    # # 3.4
+    # numCalcd = hmm.monteCarloYPathProbability([2,1,2], 10)
+    # print numCalcd, " or in percent:", float(numCalcd)/10
