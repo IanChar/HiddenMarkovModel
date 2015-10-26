74f4bd2faae8b132353bea899e3743e117c21a21
Ian Char <ianchar4@gmail.com>
2015-10-25 19:09:55 -0600

    Finished 3.3

--- HiddenMarkovModel.py
+++ HiddenMarkovModel.py
@@ -9,6 +9,26 @@ class HiddenMarkovModel(object):
         self.p = p
         self.q = q
 
+    def calculateYPermutation(self, yPerm):
+        n = len(yPerm)
+        # Generate all x permutations
+        def generatePermutation(n, numStates, currPerm = [], acc = []):
+            if len(currPerm) == numStates:
+                return [currPerm]
+            for i in range(1, numStates + 1):
+                acc += generatePermutation(n, numStates, currPerm + [i], [])
+            return acc
+        xPerms = generatePermutation(len(yPerm), int(p.shape[0]))
+        result = 0
+        for perm in xPerms:
+            component = mu.item(perm[0] - 1)
+            for i in range(len(perm) - 1):
+                component *= p.item(perm[i] - 1, perm[i+1] - 1)
+            for i in range(len(perm)):
+                component *= p.item(perm[i] - 1, yPerm[i] - 1)
+            result += component
+        return float(result)
+
     def yStatesUpTo(self, n):
         xStates = self.xStatesUpTo(n)
         yStates = []
@@ -54,3 +74,5 @@ if __name__ == '__main__':
     print hmm.xStatesUpTo(3)
     # 3.2
     print hmm.yStatesUpTo(3)
+    # 3.3
+    print hmm.calculateYPermutation([2,1,2])
